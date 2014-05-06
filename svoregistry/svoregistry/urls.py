85090db5a012d12b08ba038297924b169a13c541
Shawn Renner <shawn.renner@gmail.com>
2014-05-05 21:34:04 -0500

    downtime view

--- svoregistry/svoregistry/urls.py
+++ svoregistry/svoregistry/urls.py
@@ -2,6 +2,8 @@ from django.conf.urls import patterns, include, url
 from django.contrib import admin
 
 urlpatterns = patterns('',
+    #uncomment next line to display downtime message on home page
+    #url(r'^$', 'registry.views.coming_soon', name='home'),
     url(r'^$', 'registry.views.index', name='home'),
 
     url(r'^admin/', include(admin.site.urls)),
