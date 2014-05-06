b65b9e449fd69926d1e8ca7b06956223a40ce6a7
Shawn Renner <shawn.renner@gmail.com>
2014-05-05 21:38:36 -0500

    switch to new style url defs

--- svoregistry/svoregistry/urls.py
+++ svoregistry/svoregistry/urls.py
@@ -1,10 +1,11 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
+from registry import views
 
 urlpatterns = patterns('',
     #uncomment next line to display downtime message on home page
-    #url(r'^$', 'registry.views.coming_soon', name='home'),
-    url(r'^$', 'registry.views.index', name='home'),
+    #url(r'^$', views.coming_soon, name='home'),
+    url(r'^$', views.index, name='home'),
 
     url(r'^admin/', include(admin.site.urls)),
     
