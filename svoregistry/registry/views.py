3c07ade766ac4894e6de4d72941a469ae4b0f89c
Shawn Renner <shawn.renner@gmail.com>
2014-05-05 21:32:27 -0500

    wire up home page template

--- svoregistry/registry/views.py
+++ svoregistry/registry/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from django.template import RequestContext, loader
 
-# Create your views here.
+def coming_soon(request):
+    return HttpResponse('Welcome to the future home of the Mustang SVO registry')
+
+def index(request):
+    template = loader.get_template('index.html')
+    context = RequestContext(request)
+    return HttpResponse(template.render(context))
--- svoregistry/svoregistry/urls.py
+++ svoregistry/svoregistry/urls.py
@@ -2,9 +2,8 @@ from django.conf.urls import patterns, include, url
 from django.contrib import admin
 
 urlpatterns = patterns('',
-    # Examples:
-    # url(r'^$', 'svoregistry.views.home', name='home'),
-    # url(r'^blog/', include('blog.urls')),
+    url(r'^$', 'registry.views.index', name='home'),
 
     url(r'^admin/', include(admin.site.urls)),
+    
 )
