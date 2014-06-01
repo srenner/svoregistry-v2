13b3471c50774f8f38124e13fe4fc6426bd501e5
Shawn Renner <shawn.renner@gmail.com>
2014-06-01 11:13:48 -0500

    jquery/bootstrap modal

--- svoregistry/registry/static/registry/js/car.js
+++ svoregistry/registry/static/registry/js/car.js
@@ -3,3 +3,8 @@ var map = new google.maps.Map(document.getElementById('divMap'), {
 	center: new google.maps.LatLng(40.5, -98.4160),
 	mapTypeId: google.maps.MapTypeId.ROADMAP
 });
+
+
+var openEntryModal = function() {
+	
+};
