6c98245f93416543613123ed4c0ebef7d5b0cd65
Shawn Renner <shawn.renner@gmail.com>
2014-06-07 14:47:45 -0500

    form formatting

--- svoregistry/registry/forms.py
+++ svoregistry/registry/forms.py
@@ -8,6 +8,6 @@ class AddEntryForm(ModelForm):
         exclude = ('scrape_id', 'ip', 'entry_flag')
         widgets = {
             'car': TextInput(),
-            'comments': Textarea(attrs={'rows': 5, 'maxlength': 10000}),
+            'comments': Textarea(attrs={'rows': 5, 'cols': 120, 'maxlength': 10000}),
             'mileage': TextInput(attrs={'size': 10 }),
         }
