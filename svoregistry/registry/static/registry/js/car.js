5a1735ddc05a075ea2a4b08d4c1e3a6e834b3f17
Shawn Renner <shawn.renner@gmail.com>
2014-06-09 20:24:00 -0500

    car map

--- svoregistry/registry/static/registry/js/car.js
+++ svoregistry/registry/static/registry/js/car.js
@@ -1,8 +1,43 @@
-var map = new google.maps.Map(document.getElementById('divMap'), {
+/*var map = new google.maps.Map(document.getElementById('divMap'), {
 	zoom: 4,
 	center: new google.maps.LatLng(40.5, -98.4160),
 	mapTypeId: google.maps.MapTypeId.ROADMAP
-});
+});*/
+
+(function drawCarMap() {
+	promise.get('/map/' + activeCar + '/').then(function(error, text, xhr) {
+		if (error) {
+			return;
+		}
+		var data = JSON.parse(text);
+		var map = new google.maps.Map(document.getElementById('divMap'), {
+			zoom: 4,
+			center: new google.maps.LatLng(40.5, -98.4160),
+			mapTypeId: google.maps.MapTypeId.ROADMAP
+		});
+    	var infowindow = new google.maps.InfoWindow();
+    	var oms = new OverlappingMarkerSpiderfier(map, { keepSpiderfied: true });
+		oms.addListener('click', function(marker) {
+			infowindow.setContent(marker.desc);
+			infowindow.open(map, marker);
+		});
+		oms.addListener('spiderfy', function(markers) {
+			infowindow.close();
+		});
+		var markers = [];
+		for (var i = 0; i < data.length; i++) {
+			var marker = new google.maps.Marker({
+	        	position: new google.maps.LatLng(data[i].lat, data[i].long),
+	        	desc: "<div style='min-width: 150px'><a href='#" + data[i].entry_id + "'>" + data[i].date + "</a><br>" + data[i].owner + "</div>",
+	        	map: map
+			});
+			markers.push(marker);
+			oms.addMarker(marker);
+		}
+    	var mapOptions = {gridSize: 30, maxZoom: 7};
+    	var mc = new MarkerClusterer(map, markers, mapOptions);
+	});
+})();
 
 
 var openEntryModal = function() {
