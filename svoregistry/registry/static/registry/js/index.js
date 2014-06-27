7eb2fbca4162244c5a7f0ee96467c6a1ce12c10c
Shawn Renner <shawn.renner@gmail.com>
2014-06-26 21:01:57 -0500

    stop map scrollbar action
    Usability fix, especially for Macs

--- svoregistry/registry/static/registry/js/index.js
+++ svoregistry/registry/static/registry/js/index.js
@@ -6,6 +6,7 @@
 		var data = JSON.parse(text);
 		var map = new google.maps.Map(document.getElementById('divMap'), {
 			zoom: 4,
+			scrollwheel: false,
 			center: new google.maps.LatLng(40.5, -98.4160),
 			mapTypeId: google.maps.MapTypeId.ROADMAP
 		});
