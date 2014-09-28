e24bed8a207413ea8e7abd9a4368ab26c1397e8c
Shawn Renner <shawn.renner@gmail.com>
2014-09-28 16:21:38 -0500

    thumbnail gen and view

--- svoregistry/svoregistry/urls.py
+++ svoregistry/svoregistry/urls.py
@@ -1,6 +1,8 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
 from registry import views
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = patterns('',
     #uncomment next line to display downtime message on home page
@@ -19,3 +21,4 @@ urlpatterns = patterns('',
 
     url(r'^admin/', include(admin.site.urls)),
 )
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
