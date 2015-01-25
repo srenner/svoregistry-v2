e332d7d23a009090fffe82ca69570a21c67d725c
Shawn Renner <shawn.renner@gmail.com>
2015-01-25 15:07:42 -0600

    use jquery document ready function

--- svoregistry/registry/static/registry/js/add-entry.js
+++ svoregistry/registry/static/registry/js/add-entry.js
@@ -1,23 +1,24 @@
+$(document).ready(function() {
+	var ddlColor = document.getElementById('id_color');
+	if(ddlColor.attachEvent) {
+		ddlColor.attachEvent('onchange', colorChanged);	
+	}
+	else {
+		ddlColor.addEventListener('change', colorChanged , false);	
+	}
+	colorChanged();
+	
+	tinyMCE.init({
+	    selector: "textarea",
+	    toolbar: "bold italic bullist",
+		menubar: false,
+		statusbar: false,
+		height: 150
+	});	
+});
+
 var colorChanged = function() {
 	var ddl = document.getElementById("id_color");
 	var newColor = ddl.value;
 	ddl.className = "svo-" + newColor;
 };
-
-/* paged loaded */
-var ddlColor = document.getElementById('id_color');
-if(ddlColor.attachEvent) {
-	ddlColor.attachEvent('onchange', colorChanged);	
-}
-else {
-	ddlColor.addEventListener('change', colorChanged , false);	
-}
-colorChanged();
-
-tinyMCE.init({
-    selector: "textarea",
-    toolbar: "bold italic bullist",
-	menubar: false,
-	statusbar: false,
-	height: 150
-});
