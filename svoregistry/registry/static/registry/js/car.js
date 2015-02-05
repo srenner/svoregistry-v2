e26d09c9eb69357d5b3451a67adac4bc6d6837f2
Shawn Renner <shawn.renner@gmail.com>
2015-02-04 20:24:44 -0600

    consolidate script files on car page

--- svoregistry/registry/static/registry/js/car.js
+++ svoregistry/registry/static/registry/js/car.js
@@ -5,6 +5,23 @@ $(document).ready(function() {
     drawCarMap();
     initializeDatepicker();
     $("#btnCancel").click(function() { hideAddEntry(); });
+    
+    
+    
+	$('#id_color').change(colorChanged);
+	colorChanged();
+
+	$('#id_year').change(yearChanged);
+	yearChanged();
+	
+	tinyMCE.init({
+	    selector: "textarea",
+	    toolbar: "bold italic bullist",
+		menubar: false,
+		statusbar: false,
+		height: 150,
+		skin_url: DjangoURL.static + 'registry/css/vendor/tinymce/skins/lightgray',
+	});	
 });
 
 function hideAddEntry() {
@@ -54,4 +71,57 @@ function initializeDatepicker() {
         }
     });
     picker.setMoment(moment());
+};
+
+
+
+var colorChanged = function() {
+	var ddl = document.getElementById("id_color");
+	var newColor = ddl.value;
+	ddl.className = "svo-" + newColor;
+};
+
+var yearChanged = function() {
+	var ddlColor = $('#id_color');
+	var selectedColor = ddlColor.val();
+	ddlColor.empty();
+	var colorOptions = svoColorChoices($('#id_year').val());
+	for(var i = 0; i < colorOptions.length; i++) {
+		ddlColor
+			.append($("<option></option>")
+			.attr("value", colorOptions[i].code)
+			.text(colorOptions[i].descr));
+		ddlColor.val(selectedColor);
+	}
+	if(!ddlColor.val()) {
+		ddlColor.val(colorOptions[0].code);
+		colorChanged();
+	}
+};
+
+var addEntryAjax = function() {
+	tinyMCE.triggerSave();
+	if(!window.FormData) {
+		return true;
+	}
+	//if we made it this far we can perform file upload via ajax	
+	var fd = new FormData(document.getElementById("formAdd"));
+	$.ajax({
+		url: "/add",
+		data: fd,
+		processData: false,
+		contentType: false,
+		type: 'POST',	  
+		success: 
+			function(data) {
+				hideAddEntry();
+				var divEntries = document.getElementById("divEntries");
+				if(divEntries) {
+					var newEntry = document.createElement("div");
+					newEntry.innerHTML = data;
+					divEntries.insertBefore(newEntry, divEntries.firstChild);
+				}
+			}
+	});
+	return false;
 };
