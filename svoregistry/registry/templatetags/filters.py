9fc78113390c686175c7f7d3348231a662cfb62a
Shawn Renner <shawn.renner@gmail.com>
2015-02-14 15:14:42 -0600

    search engine upgrades

--- svoregistry/registry/templatetags/filters.py
+++ svoregistry/registry/templatetags/filters.py
@@ -4,6 +4,13 @@ from django.template.defaultfilters import stringfilter
 register = template.Library()
 
 @register.filter
-
 def color_lookup(tuple, key):
     return dict(tuple).get(key, "Unknown Color")
+
+@register.filter
+def state_lookup(tuple, key):
+    return dict(tuple).get(key, "")
+
+@register.filter
+def country_lookup(tuple, key):
+    return dict(tuple).get(key, "")
