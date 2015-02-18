d7e7c5de4f2ab996417798b97031dfaf95671c89
Shawn Renner <shawn.renner@gmail.com>
2015-02-17 22:08:42 -0600

    robots.txt initial setup

--- svoregistry/svoregistry/urls.py
+++ svoregistry/svoregistry/urls.py
@@ -3,6 +3,7 @@ from django.contrib import admin
 from registry import views
 from django.conf import settings
 from django.conf.urls.static import static
+from django.views.generic import TemplateView
 
 urlpatterns = patterns('',
     #url(r'^$', views.coming_soon, name='home'),
@@ -38,6 +39,10 @@ urlpatterns = patterns('',
 
     url(r'^admin/', include(admin.site.urls)),
     url(r'^search/', include('haystack.urls')),
+    
+    url(r'^robots\.txt$', TemplateView.as_view(template_name='robots.txt', content_type='text/plain')),
+    
+    
 )
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
